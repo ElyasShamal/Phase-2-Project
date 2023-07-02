96c6927cef2322c59a7a62ffe36fff12e8b6be99
Elyas <Elyas.shamal2@gmail.com>
2023-07-02 16:57:44 -0400

    make a fetch request

---
+++ movie-app/src/components/Movie.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+function Movie() {
+
+    const [movies.setMovies] = useState(null);
+  useEffect(() => {
+    const fetchMovie = () => {
+      fetch("http://localhost:3333");
+    };
+  });
+
+  return (
+    <div>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default Movie;
