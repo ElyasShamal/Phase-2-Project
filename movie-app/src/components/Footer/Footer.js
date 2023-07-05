e9ff403f6b91a3bea711781fc2bcca98aa7d6180
Elyas <Elyas.shamal2@gmail.com>
2023-07-05 04:49:17 -0400

    create footer.js

--- movie-app/src/components/Footer/Footer.js
+++ movie-app/src/components/Footer/Footer.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
+  const links = [
+    {
+      title: "LinkedIn",
+      url: "https://www.linkedin.com/in/mohammad-elyas-shamal-b975b7275/",
+    },
+    { title: "GitHub", url: "https://github.com/ElyasShamal" },
+  ];
+
   return (
     <footer>
       <ul>
-        <a>
-          <li></li>
-        </a>
-        <a>
-          <li></li>
-        </a>
-        <a>
-          <li></li>
-        </a>
+        {links.map((link, index) => (
+          <a key={index} href={link.url}>
+            <li>
+              {link.title === "LinkedIn" && <FaLinkedin />}
+              {link.title === "GitHub" && <FaGithub />}
+              {link.title}
+            </li>
+          </a>
+        ))}
       </ul>
     </footer>
   );
